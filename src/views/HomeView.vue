8cab7abf1bdf7c3c26481d73b49f66458d8aa485
Gim128 <gimhanpabasara4@gmail.com>
2023-02-20 14:59:11 +0530

    create navbar for meals:done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,18 +9,32 @@
     </div>
 
     <div class="flex gap-2 mt-2 justify-center">
-      <router-link v-for="letter in letters" :key="letter" to="/">
+      <router-link
+        v-for="letter in letters"
+        :key="letter"
+        to="{name: 'byLetter', params: {letter}}"
+      >
         {{ letter }}
       </router-link>
     </div>
+
+    <!-- <pre>{{ ingredients }}</pre> -->
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient.js";
 
-const meals = computed(() => store.state.meals);
+// const meals = computed(() => store.state.meals);
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log(response.data);
+  ingredients.value = response.data;
+});
 </script>
 
